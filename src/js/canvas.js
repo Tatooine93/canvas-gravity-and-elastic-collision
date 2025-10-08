0ce4c4fb9905639d0810f5d6e52e28dbf6607579
Antoine <nowakantoine93@gmail.com>
2025-10-07 21:20:52 -0600

    upgrade design and control

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,4 @@
-import utils, { randomIntFromRange, randomColor, distance, rotate, resolveCollision } from './utils'
+/* import utils, { randomIntFromRange, randomColor, distance, rotate, resolveCollision } from './utils'
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -84,13 +84,13 @@ class Ball {
       this.velocity.x = this.velocity.x * airFriction
     }
 
-    /* if(this.y > canvas.height || this.y <= 0 || this.x > canvas.width || this.x <= 0) {
-      document.body.style.backgroundColor = 'red'
-    }
-    else {
-      document.body.style.backgroundColor = 'white'
-    }
- */
+    //if(this.y > canvas.height || this.y <= 0 || this.x > canvas.width || this.x <= 0) {
+      //document.body.style.backgroundColor = 'red'
+    //}
+    //else {
+    //  document.body.style.backgroundColor = 'white'
+    //}
+
     this.x += this.velocity.x;
     this.y += this.velocity.y;
 
@@ -143,3 +143,149 @@ function animate() {
 
 init()
 animate()
+ */
+
+import { randomIntFromRange, randomColor, distance, rotate, resolveCollision } from './utils.js';
+
+const canvas = document.getElementById('simCanvas');
+const c = canvas.getContext('2d');
+
+let settings = {
+  gravity: 0.2,
+  friction: 0.96,
+  airFriction: 0.99999,
+  ballsNumber: 60,
+  minRadius: 8,
+  maxRadius: 20
+};
+
+let ballArray = [];
+
+class Ball {
+  constructor(x, y, dx, dy, radius, color, mass) {
+    this.x = x;
+    this.y = y;
+    this.velocity = { x: dx, y: dy };
+    this.radius = radius;
+    this.color = color;
+    this.mass = mass;
+  }
+
+  draw() {
+    const gradient = c.createRadialGradient(
+      this.x - this.radius / 3,
+      this.y - this.radius / 3,
+      this.radius / 5,
+      this.x,
+      this.y,
+      this.radius
+    );
+    gradient.addColorStop(0, '#fff');
+    gradient.addColorStop(0.3, this.color);
+    gradient.addColorStop(1, '#000');
+
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = gradient;
+    c.fill();
+    c.closePath();
+  }
+
+  update(ballArray) {
+    for (let ball of ballArray) {
+      if (this === ball) continue;
+      if (distance(this.x, this.y, ball.x, ball.y) - this.radius * 2 < 0) {
+        resolveCollision(this, ball);
+      }
+    }
+
+    if (this.y + this.radius + this.velocity.y > canvas.height || this.y - this.radius <= 0) {
+      this.velocity.y = -this.velocity.y * settings.friction;
+      this.velocity.x *= settings.friction;
+    } else {
+      this.velocity.y += settings.gravity;
+    }
+
+    if (this.x + this.radius > canvas.width || this.x - this.radius <= 0) {
+      this.velocity.x = -this.velocity.x;
+    } else {
+      this.velocity.x *= settings.airFriction;
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.draw();
+  }
+}
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function init() {
+  ballArray = [];
+  for (let i = 0; i < settings.ballsNumber; i++) {
+    let radius = randomIntFromRange(settings.minRadius, settings.maxRadius);
+    let x = randomIntFromRange(radius, canvas.width - radius);
+    let y = randomIntFromRange(radius, canvas.height - radius);
+    let dx = randomIntFromRange(-2, 2);
+    let dy = randomIntFromRange(-2, 2);
+    let color = randomColor(['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']);
+
+    if (i !== 0) {
+      for (let j = 0; j < ballArray.length; j++) {
+        if (distance(x, y, ballArray[j].x, ballArray[j].y) - radius * 2 < 0) {
+          x = randomIntFromRange(radius, canvas.width - radius);
+          y = randomIntFromRange(radius, canvas.height - radius);
+          j = -1;
+        }
+      }
+    }
+    ballArray.push(new Ball(x, y, dx, dy, radius, color, 1));
+  }
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  for (const ball of ballArray) {
+    ball.update(ballArray);
+  }
+}
+
+function restartSimulation() {
+  init();
+}
+
+window.addEventListener('resize', () => {
+  resizeCanvas();
+  init();
+});
+
+window.addEventListener('load', () => {
+  resizeCanvas();
+  init();
+  animate();
+});
+
+// --- Sliders ---
+const inputs = [
+  "gravity",
+  "friction",
+  "airFriction",
+  "ballsNumber",
+  "minRadius",
+  "maxRadius"
+];
+
+inputs.forEach((key) => {
+  const input = document.getElementById(key);
+  const valueLabel = document.getElementById(`val-${key}`);
+  input.addEventListener("input", (e) => {
+    settings[key] = parseFloat(e.target.value);
+    valueLabel.textContent = e.target.value;
+  });
+});
+
+document.getElementById("restartBtn").addEventListener("click", restartSimulation);
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-function randomIntFromRange(min, max) {
+/* function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
@@ -61,3 +61,56 @@ function resolveCollision(particle, otherParticle) {
 }
 
 module.exports = { randomIntFromRange, randomColor, distance, rotate, resolveCollision }
+ */
+
+
+
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function distance(x1, y1, x2, y2) {
+  const xDist = x2 - x1;
+  const yDist = y2 - y1;
+  return Math.sqrt(xDist ** 2 + yDist ** 2);
+}
+
+function rotate(velocity, angle) {
+  return {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
+}
+
+function resolveCollision(p1, p2) {
+  const xVelocityDiff = p1.velocity.x - p2.velocity.x;
+  const yVelocityDiff = p1.velocity.y - p2.velocity.y;
+  const xDist = p2.x - p1.x;
+  const yDist = p2.y - p1.y;
+
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+    const angle = -Math.atan2(p2.y - p1.y, p2.x - p1.x);
+    const m1 = p1.mass;
+    const m2 = p2.mass;
+
+    const u1 = rotate(p1.velocity, angle);
+    const u2 = rotate(p2.velocity, angle);
+
+    const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+    const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+    const vFinal1 = rotate(v1, -angle);
+    const vFinal2 = rotate(v2, -angle);
+
+    p1.velocity.x = vFinal1.x;
+    p1.velocity.y = vFinal1.y;
+    p2.velocity.x = vFinal2.x;
+    p2.velocity.y = vFinal2.y;
+  }
+}
+
+export { randomIntFromRange, randomColor, distance, rotate, resolveCollision };
